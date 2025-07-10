248d88b4d67f4596804e7228795a24ff71058375
ReallySoggySocks <havenkain@gmail.com>
2025-07-10 00:11:20 -0500

    completed get_files_info function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,15 +2,24 @@ import os
 
 def get_files_info(working_directory, directory=None):
     full_path = os.path.join(working_directory, directory)
-    if working_directory not in os.path.abspath(full_path):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(directory):
-        return f'Error: "{directory}" is not a directory'
-    else:
-        print(os.listdir(full_path))
-        str_list = []
-        for file in os.listdir(full_path):
-            str_list.append(f'- {file}: file_size={os.path.getsize(file)}, is_dir={os.path.isdir(file)}')
+    abs_wd = os.path.abspath(working_directory)
+    abs_full_path = os.path.abspath(full_path)
 
+    str_list = []
+    if directory == ".":
+        str_list.append("Result for current directory:")
+    else:
+        str_list.append(f"Result for '{directory}' directory:")
+
+    if not abs_full_path.startswith(abs_wd):
+        str_list.append(f'  Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        return "\n".join(str_list)
+    if not os.path.isdir(abs_full_path):
+        str_list.append(f'  Error: "{directory}" is not a directory')
+        return "\n".join(str_list)
+    else:
+        for item in os.listdir(full_path):
+            item_path = os.path.join(full_path, item)
+            str_list.append(f'- {item}: file_size={os.path.getsize(item_path)}, is_dir={os.path.isdir(item_path)}')
         return "\n".join(str_list)
     
--- tests.py
+++ tests.py
@@ -2,8 +2,21 @@ import unittest
 from functions.get_files_info import get_files_info
 
 class TestGetFiles(unittest.TestCase):
-    def testcalc(slef):
-        print(get_files_info("calculator", "."))
+    def testcalc(self):
+        pass
+        #print(f"{get_files_info("calculator", ".")}\n")
+    
+    def testpkg(self):
+        pass
+        #print(f"{get_files_info("calculator", "pkg")}\n")
+
+    def testbin(self):
+        pass
+        #print(f"{get_files_info("calculator", "/bin")}\n")
+
+    def testnondir(self):
+        pass
+        #print(f"{get_files_info("calculator", "../")}\n")
 
 if __name__ == "__main__":
     unittest.main()
